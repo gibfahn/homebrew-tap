1ff0fe60560facf4dc93b186e4394aca50c1918a
Gibson Fahnestock <gibfahn@gmail.com>
2021-04-13 12:41:54 +0100

    feat(check-sieve): add a formula for sieve checking CLI

---
+++ Formula/check-sieve.rb
@@ -0,0 +1,13 @@
+class CheckSieve < Formula
+  version "0.6"
+  sha256 "e5d339258ae423e3bcc81142e211f4f448af288b089b0630240f8473adeac2c5"
+
+  desc "CLI to check Sieve files for errors."
+  homepage "https://github.com/dburkart/check-sieve"
+  url "https://github.com/dburkart/check-sieve/archive/refs/tags/check-sieve-#{version}.tar.gz"
+  license "MIT"
+
+  def install
+    system "make", "install", "INSTALL_PREFIX=#{prefix}"
+  end
+end
