1094828288105803a2fd9105c2c8abc4a4253dbb
Gibson Fahnestock <gibfahn@gmail.com>
2021-10-23 00:59:17 +0100

    chore(check-sieve): update to 0.7

--- Formula/check-sieve.rb
+++ Formula/check-sieve.rb
@@ -1,14 +1,14 @@
 # See upstream issue: https://github.com/dburkart/check-sieve/issues/46
 class CheckSieve < Formula
-  version "0.6"
-  sha256 "e5d339258ae423e3bcc81142e211f4f448af288b089b0630240f8473adeac2c5"
+  version '0.7'
+  sha256 '5a51817cf9da205d56e3bd0bf9e7a87f3ee7005b3edf803b6752dc019735c994'
 
-  desc "CLI to check Sieve files for errors."
-  homepage "https://github.com/dburkart/check-sieve"
+  desc 'CLI to check Sieve files for errors.'
+  homepage 'https://github.com/dburkart/check-sieve'
   url "https://github.com/dburkart/check-sieve/archive/refs/tags/check-sieve-#{version}.tar.gz"
-  license "MIT"
+  license 'MIT'
 
   def install
-    system "make", "install", "INSTALL_PREFIX=#{prefix}"
+    system 'make', 'install', "INSTALL_PREFIX=#{prefix}"
   end
 end
