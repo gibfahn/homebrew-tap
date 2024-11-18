fc66b3e7984ff752e9a0b8436dad94724c6d2d98
Gibson Fahnestock <gibfahn@gmail.com>
2024-11-18 14:19:42 +0000

    chore(check-sieve): update to version 0.10

--- Formula/check-sieve.rb
+++ Formula/check-sieve.rb
@@ -1,7 +1,7 @@
 # See upstream issue: https://github.com/dburkart/check-sieve/issues/46
 class CheckSieve < Formula
-  version '0.9'
-  sha256 'cc9a8ee4fe3e6464d1934325d2c17c4f37799aedb85d7fda9e00991e23d93c60'
+  version '0.10'
+  sha256 '8cac6b8b7120d6c230c705465f95220bd52578e2f1775de8963dc04c0abf8d4e'
 
   desc 'CLI to check Sieve files for errors.'
   homepage 'https://github.com/dburkart/check-sieve'
